91ec4a5ce1cdf07dd41d7cfad3df5d29573946ae
Joy <146790408+joy-totheworld@users.noreply.github.com>
2024-06-10 00:14:49 -0700

    working version

--- Who_is_caller_10.js
+++ Who_is_caller_10.js
@@ -1,8 +1,10 @@
 
 var posts = document.getElementsByClassName("caller-avatar-default");
 var callingAvatar = document.getElementById("caller-avatar-calling");
+const startButton = document.getElementById("start-button");
+
 // console.log(callingAvatar)
-console.log (callingAvatar.innerHTML)
+// console.log (callingAvatar.innerHTML)
 // console.log(posts[Math.floor(Math.random() * (posts.length))])
 // console.log(Math.floor(Math.random() * (posts.length)));
 
@@ -11,7 +13,17 @@ var callerNumber10 = "";
 var currcaller = null;
 var originalAvatar = null;
 
-window.setTimeout(displayCallingAvatar, 1100)
+// window.setTimeout(displayCallingAvatar, 1100)
+startButton.addEventListener("click", start);
+
+
+function start () {
+    const backdrop = document.getElementById("modal-backdrop");
+    backdrop.classList.add("hidden");
+    const modal = document.getElementById("modal");
+    modal.classList.add("hidden");
+    displayCallingAvatar ()
+}
 
 function displayCallingAvatar (){
     revertAvatar ()
@@ -24,7 +36,8 @@ function displayCallingAvatar (){
     }
     currcaller.classList.remove("caller-avatar-default");
     originalAvatar = currcaller.innerHTML
-    console.log(originalAvatar)
+    // console.log(originalAvatar)
+    console.log(currcaller.id)
     currcaller.innerHTML = callingAvatar.innerHTML
     currcaller.classList.add("calling-avatar");
     console.log(posts.length)
@@ -33,6 +46,8 @@ function displayCallingAvatar (){
     }
     else {
         window.setTimeout(revertAvatar, 1100)
+        console.log("caller number 10: ", callerNumber10)
+        getAnswer ()
     }
 }
 
@@ -44,4 +59,36 @@ function revertAvatar (){
     }
 }
 
-console.log(callerNumber10)
+function getAnswer () {
+    const backdrop = document.getElementById("modal-backdrop");
+    backdrop.classList.remove("hidden");
+    const modalText = document.getElementById("modal-text");
+    modalText.innerHTML = "Who was caller number 10? <br> Enter the lowercase alphabet identifier of the winning bear."
+    const startButton = document.getElementById("start-button");
+    startButton.classList.add("hidden");
+    const submitButton = document.getElementById("submit-button");
+    submitButton.classList.remove("hidden");
+    const answerInput = document.getElementById("answer-input");
+    answerInput.classList.remove("hidden");
+    const modal = document.getElementById("modal");
+    modal.classList.remove("hidden");
+    submitButton.addEventListener("click", validate);
+}
+
+function validate () {
+    const answerInput = document.getElementById("answer-input");
+    answerInput.classList.add("hidden");
+    const submitButton = document.getElementById("submit-button");
+    submitButton.classList.add("hidden");
+    const modalText = document.getElementById("modal-text");
+    if (answerInput.value == callerNumber10) {
+        console.log(answerInput.value)
+        console.log(callerNumber10)
+        modalText.innerHTML = "You are correct!"
+    }
+    else {
+        console.log(answerInput.value)
+        console.log(callerNumber10)
+        modalText.innerHTML = "That poor bear only wishes he won the jar..."
+    }
+}
